cca85a543996afdf50d799ab4a9d52095836c382
feliperost <87390724+feliperost@users.noreply.github.com>
2022-06-27 16:43:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -63,3 +63,7 @@ setValues();
 function showCss() {
   cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
 }
+
+function resetStorage() {
+  localStorage.clear();
+}
